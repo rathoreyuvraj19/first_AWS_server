1eea40703cef95acd8c7f4156efdc6968fc81e85
Yuvraj Singh Rathore <rathore.yuvraj19@gmail.com>
2024-03-13 17:02:55 +0530

    fourth_commit

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ import express from "express";
 
 const app = express();
 // const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
